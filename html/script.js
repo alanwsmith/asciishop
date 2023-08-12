da2456b0f9a1ad5e1d22a1de48c74ff802f9c3fe
Alan Smith <commit@alanwsmith.com>
2023-08-11 20:02:45 -0400

    - got layer wokring again again

--- html/script.js
+++ html/script.js
@@ -1,150 +1,156 @@
 let s = {
-    tables: [],
-    currentTable: 0,
-    currentCol: 0,
-    currentRow: 0,
-    cols: 80,
-    rows: 50,
-    selectCoord: {t: null, r: null, c: null},
-    freqChars: [],
-    visibleLayers: [],
-  }
+  tables: [],
+  currentTable: 0,
+  currentCol: 0,
+  currentRow: 0,
+  cols: 80,
+  rows: 50,
+  selectCoord: { t: null, r: null, c: null },
+  freqChars: [],
+  visibleLayers: [],
+}
 
-  const addCharacter = (event) => {
-    console.log("addCharacter")
-  }
+const addCharacter = (event) => {
+  console.log("addCharacter")
+}
 
-  const charClickHandler = () => {
-    console.log("charClickHandler")
-  }
+const getId = () => {
+  return `t${s.currentTable}_c${s.currentCol}_r${s.currentRow}`
+}
 
-  const getId = () => {
-    return `t${s.currentTable}_c${s.currentCol}_r${s.currentRow}`
-  }
+const handleCharClick = (event) => {
+  console.log(event.srcElement.innerText)
+  s.tables[s.currentTable][s.currentCol][s.currentRow] = event.srcElement.innerText
+  renderLayers()
+}
 
-  const handlePixelClick = (event) => {
-    console.log("pixel click")
-    let data = event.srcElement.dataset
-    s.currentCol = parseInt(data.col, 10)
-    s.currentRow = parseInt(data.row, 10)
-    updateStyles()
-  }
 
-  const importAscii = () => {
-    console.log("importAscii")
-    // build the base table to ensure it's full
-    let newTable = []
-    console.log(s.cols)
-    for (let c = 0; c < s.cols; c += 1) {
-      console.log("-")
-      let newCol = []
-      for (let r = 0; r < s.rows; r += 1) {
-        newCol.push(" ")
-      }
-      newTable.push(newCol)
+const handlePixelClick = (event) => {
+  console.log("pixel click")
+  let data = event.srcElement.dataset
+  s.currentCol = parseInt(data.col, 10)
+  s.currentRow = parseInt(data.row, 10)
+  updateStyles()
+}
+
+const importAscii = () => {
+  console.log("importAscii")
+  // build the base table to ensure it's full
+  let newTable = []
+  console.log(s.cols)
+  for (let c = 0; c < s.cols; c += 1) {
+    console.log("-")
+    let newCol = []
+    for (let r = 0; r < s.rows; r += 1) {
+      newCol.push(" ")
     }
-    // then populate it
-    let rows = copyArea.innerText.split("\n")
-    console.log(rows)
-    rows.forEach((row, rowIndex) => {
-      let cols = row.split("")
-      cols.forEach((char, colIndex) => {
-        newTable[colIndex][rowIndex] = char
-      })
+    newTable.push(newCol)
+  }
+  // then populate it
+  let rows = copyArea.innerText.split("\n")
+  console.log(rows)
+  rows.forEach((row, rowIndex) => {
+    let cols = row.split("")
+    cols.forEach((char, colIndex) => {
+      newTable[colIndex][rowIndex] = char
     })
-    s.tables.push(newTable)
-    s.visibleLayers.push(true)
-    s.currentTable = s.tables.length - 1
-    renderLayers()
-  }
+  })
+  s.tables.push(newTable)
+  s.visibleLayers.push(true)
+  s.currentTable = s.tables.length - 1
+  renderLayers()
+}
 
-  const keydownHandler = () => {
-    console.log("keydownHandler")
-  }
+const keydownHandler = () => {
+  console.log("keydownHandler")
+}
 
-  const layerToggleHandler = (event) => {
-    console.log(event.srcElement)
-    console.log(event.srcElement.dataset.layer)
-    s.visibleLayers[event.srcElement.dataset.layer] = !s.visibleLayers[event.srcElement.dataset.layer]
-    console.log(s.visibleLayers)
-    renderLayers()
-  }
+const layerToggleHandler = (event) => {
+  console.log(event.srcElement)
+  console.log(event.srcElement.dataset.layer)
+  s.visibleLayers[event.srcElement.dataset.layer] = !s.visibleLayers[event.srcElement.dataset.layer]
+  console.log(s.visibleLayers)
+  renderLayers()
+}
 
-  const prepChars = () => {
-    console.log("prepChars")
-    const chars = document.getElementsByClassName("char")
-    for (c = 0; c < chars.length; c += 1) {
-      chars[c].addEventListener("click", addCharacter)
+const prepChars = () => {
+  console.log("prepChars")
+  const chars = document.getElementsByClassName("char")
+  for (c = 0; c < chars.length; c += 1) {
+    chars[c].addEventListener("click", addCharacter)
+  }
+}
+
+const renderLayers = () => {
+  const l = s.tables.length
+  console.log("renderLayers")
+  while (bg.children.length > 0) {
+    bg.children[0].remove()
+  }
+  while (layerToggles.children.length > 0) {
+    layerToggles.children[0].remove()
+  }
+  s.tables.forEach((table, tableIndex) => {
+    let renderTable = document.createElement("table")
+    let layerToggle = document.createElement("input")
+    layerToggle.type = "checkbox"
+    layerToggle.dataset.layer = tableIndex
+    layerToggle.addEventListener("change", layerToggleHandler)
+    if (s.visibleLayers[tableIndex]) {
+      layerToggle.checked = true;
     }
-  }
-
-  const renderLayers = () => {
-    const l = s.tables.length
-    console.log("renderLayers")
-    while (bg.children.length > 0) {
-      bg.children[0].remove()
+    layerToggles.appendChild(layerToggle)
+    renderTable.classList.add("layerTable")
+    if (tableIndex == s.currentTable) {
+      renderTable.classList.add("activeTable")
+    } else {
+      renderTable.classList.add("inactiveTable")
     }
-    while (layerToggles.children.length > 0) {
-      layerToggles.children[0].remove()
-    }
-    s.tables.forEach((table, tableIndex) => {
-      let renderTable = document.createElement("table")
-      let layerToggle = document.createElement("input")
-      layerToggle.type = "checkbox"
-      layerToggle.dataset.layer = tableIndex
-      layerToggle.addEventListener("change", layerToggleHandler)
-      if (s.visibleLayers[tableIndex]) {
-        layerToggle.checked = true;
-      }
-      layerToggles.appendChild(layerToggle)
-      renderTable.classList.add("layerTable")
-      if (tableIndex == s.currentTable) {
-        renderTable.classList.add("activeTable")
-      } else {
-        renderTable.classList.add("inactiveTable")
-      }
-      for (let r = 0; r < s.rows; r++) {
-        let tableRow = document.createElement("tr")
-        renderTable.appendChild(tableRow)
-        for (let c = 0; c < s.cols; c++) {
-          let tableCell = document.createElement("td")
-          tableCell.classList.add("pixelCell")
-          let cellButton = document.createElement("button")
-          cellButton.dataset.row = r
-          cellButton.dataset.col = c
+    for (let r = 0; r < s.rows; r++) {
+      let tableRow = document.createElement("tr")
+      renderTable.appendChild(tableRow)
+      for (let c = 0; c < s.cols; c++) {
+        let tableCell = document.createElement("td")
+        tableCell.classList.add("pixelCell")
+        let cellButton = document.createElement("button")
+        cellButton.dataset.row = r
+        cellButton.dataset.col = c
 
-          cellButton.id = `t${tableIndex}_c${c}_r${r}`
-          if (s.visibleLayers[tableIndex]) {
-            cellButton.innerHTML = s.tables[tableIndex][c][r]
-          }
-          cellButton.classList.add("pixel")
-          tableCell.appendChild(cellButton)
-          tableRow.appendChild(tableCell)
+        cellButton.id = `t${tableIndex}_c${c}_r${r}`
+        if (s.visibleLayers[tableIndex]) {
+          cellButton.innerHTML = s.tables[tableIndex][c][r]
         }
+        cellButton.classList.add("pixel")
+        tableCell.appendChild(cellButton)
+        tableRow.appendChild(tableCell)
       }
-      bg.appendChild(renderTable)
-    })
-    updateStyles()
-  }
+    }
+    bg.appendChild(renderTable)
+  })
+  updateStyles()
+}
 
-  updateStyles = () => {
-    const upperIndex = s.tables.length - 1
-    s.tables[upperIndex].forEach((r, rIndex) => {
-      r.forEach((c, cIndex) => {
-        const theId = `t${upperIndex}_c${cIndex}_r${rIndex}`
-        const el = document.getElementById(theId)
+updateStyles = () => {
+  const upperIndex = s.tables.length - 1
+  s.tables[upperIndex].forEach((r, rIndex) => {
+    r.forEach((c, cIndex) => {
+      const theId = `t${upperIndex}_c${cIndex}_r${rIndex}`
+      const el = document.getElementById(theId)
+      if (el !== null) {
         if (cIndex === s.currentCol && rIndex === s.currentRow) {
           el.classList.add("activePixel")
         } else {
           el.classList.remove("activePixel")
         }
-      })
+      }
     })
-  }
-
-  document.addEventListener("DOMContentLoaded", () => {
-    bg.addEventListener("click", handlePixelClick)
-    importButton.addEventListener("click", importAscii)
-    document.addEventListener("keydown", keydownHandler)
-    prepChars()
   })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  bg.addEventListener("click", handlePixelClick)
+  charContainer.addEventListener("click", handleCharClick)
+  importButton.addEventListener("click", importAscii)
+  document.addEventListener("keydown", keydownHandler)
+  prepChars()
+})
